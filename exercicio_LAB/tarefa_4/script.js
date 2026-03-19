6129adf6f5a95907d252e1229e5c557215f7f802
Veronica Ribeiro <veronica.rib16@gmail.com>
2026-03-18 22:26:18 -0300

    funcao

--- exercicio_LAB/tarefa_4/script.js
+++ exercicio_LAB/tarefa_4/script.js
@@ -1,7 +1,8 @@
-var x = document.getElementById("number");
+var x = document.getElementById("number").value;
 
 var y = Math.floor(Math.random());
 
+DarAviso(){
 if(x==y){
     console.log("São iguais");
     document.getElementById("fundo").style.setProperty("background-color","green");
@@ -12,5 +13,6 @@ if(x==y){
    console.log("São diferentes");
    document.getElementById("fundo").style.setProperty("background-color","red"); 
 }else{
-    console("Algo deu errado");
+    console.log("Algo deu errado");
+}
 }
