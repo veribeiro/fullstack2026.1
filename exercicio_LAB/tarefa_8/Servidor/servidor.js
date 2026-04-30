3ad2e845f8913cd9f82a45efe253d7d98b87bf5d
Veronica Ribeiro <veronica.rib16@gmail.com>
2026-04-29 22:19:26 -0300

    tarefa 8

---
+++ exercicio_LAB/tarefa_8/Servidor/servidor.js
@@ -0,0 +1,37 @@
+var http = require('http');
+var express = require('express');
+var colors = require('colors');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(express.static('./public'));
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.json())
+app.set('view engine', 'ejs')
+app.set('views', './views');
+
+var server = http.createServer(app);
+server.listen(80);
+
+console.log('Servidor rodando ...'.rainbow);
+
+app.get('/', function (requisicao, resposta){
+resposta.redirect('Login.html')
+})
+
+app.get('/login', function (requisicao, resposta){
+var nome = requisicao.query.info;
+var senha = requisicao.query.info;
+console.log(nome);
+})
+
+app.get('/cadastro',function (requisicao, resposta){
+var nome = requisicao.query.nome;
+var sobrenome = requisicao.query.sobrenome;
+var nascimento = requisicao.query.nascimento;
+var civil = requisicao.query.civil;
+var senha = requisicao.query.info;
+
+resposta.render('resposta_cadastro', {nome, sobrenome, nascimento, civil, senha})
+resposta.render('resposta_login', {nome, senha})
+})
